1393ea92aa6a669bc988b7de8b126115ea0e8aad
WilliamCodyz <ddinhphat9@gmail.com>
2026-01-26 14:33:54 +0700

    replace check length password 8->6

--- labodc-web-portal/src/pages/auth/Register/Register.tsx
+++ labodc-web-portal/src/pages/auth/Register/Register.tsx
@@ -73,7 +73,7 @@ const Register: React.FC = () => {
               name="password"
               rules={[
                 { required: true, message: 'Vui lòng nhập mật khẩu!' },
-                { min: 8, message: 'Mật khẩu phải có ít nhất 8 ký tự!' },
+                { min: 6, message: 'Mật khẩu phải có ít nhất 6 ký tự!' },
               ]}
             >
               <Input.Password
