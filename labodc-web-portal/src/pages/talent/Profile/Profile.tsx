5a605c526a7bef7bcc50272cd426eaf72eff6e11
caoduc505 <huynhcaoduc.270305@gmail.com>
2026-01-28 11:33:53 +0700

    add profile

--- labodc-web-portal/src/pages/talent/Profile/Profile.tsx
+++ labodc-web-portal/src/pages/talent/Profile/Profile.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Avatar, Descriptions } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
 const Profile: React.FC = () => {
   return (
-    <div>
-      <h1 className="page-title">Hồ sơ cá nhân</h1>
-      <Card><p>Thông tin hồ sơ...</p></Card>
+    <div style={{ padding: 24 }}>
+        
+      <h1> Hồ sơ cá nhân </h1>
+
+      <Card>
+        <Avatar
+          size={80}
+          icon={<UserOutlined />}
+          style={{ marginBottom: 16 }}
+        />
+
+        <Descriptions column={1} bordered>
+          <Descriptions.Item label="Họ tên">
+            Huỳnh Cao Đức
+          </Descriptions.Item>
+          <Descriptions.Item label="Vai trò">
+            Talent / Sinh viên
+          </Descriptions.Item>
+          <Descriptions.Item label="Email">
+            duc@example.com
+          </Descriptions.Item>
+          <Descriptions.Item label="Kỹ năng">
+            React, TypeScript, Java, SQL
+          </Descriptions.Item>
+        </Descriptions>
+      </Card>
     </div>
   );
 };
