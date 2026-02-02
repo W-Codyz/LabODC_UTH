445720dcc9cc18ff91341d485e3bba8d8339bf3d
WilliamCodyz <ddinhphat9@gmail.com>
2026-02-03 00:10:36 +0700

    edit proxy config

--- labodc-web-portal/vite.config.ts
+++ labodc-web-portal/vite.config.ts
@@ -26,18 +26,6 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
-      '/auth': {
-        target: 'http://localhost:8085',
-        changeOrigin: true,
-      },
-      '/users': {
-        target: 'http://localhost:8085',
-        changeOrigin: true,
-      },
-      '/projects': {
-        target: 'http://localhost:8085',
-        changeOrigin: true,
-      },
       '/actuator': {
         target: 'http://localhost:8085',
         changeOrigin: true,
